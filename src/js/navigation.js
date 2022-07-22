287b527e371d2abae775ec65069cbac80268f5d3
SeanTOSCD <sean@toscd.com>
2022-07-22 10:03:08 -0500

    mobile menu modal progress

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -80,9 +80,14 @@
 		menu.classList.add('nav-menu');
 	}
 
+	const menuContainer = document.querySelector("#site-navigation > [class*='-container']");
+
 	// Toggle the .toggled class and the aria-expanded value each time the button is clicked.
 	button.addEventListener('click', function() {
+
 		siteNavigation.classList.toggle('toggled');
+
+		menuContainer.classList.add("overlay-menu-active");
 
 		if (button.getAttribute('aria-expanded') === 'true') {
 			button.setAttribute('aria-expanded', 'false');
@@ -91,13 +96,25 @@
 		}
 	});
 
-	// Remove the .toggled class and set aria-expanded to false when the user clicks outside the navigation.
-	document.addEventListener('click', function(event) {
-		const isClickInside = siteNavigation.contains(event.target);
+	function closeMenuContainer() {
+		menuContainer.classList.remove("overlay-menu-active");
+		siteNavigation.classList.remove("toggled");
+		button.setAttribute("aria-expanded", "false");
+	}
 
-		if (!isClickInside) {
-			siteNavigation.classList.remove('toggled');
-			button.setAttribute('aria-expanded', 'false');
+	menuContainer.addEventListener("click", function(event) {
+
+		const isClickOutside = !event.target.closest(".nav-menu");
+
+		if (isClickOutside) {
+			closeMenuContainer();
+		}
+	});
+
+	window.addEventListener("keydown", (event) => {
+		console.log(event);
+		if (event.key === "Escape") {
+			closeMenuContainer();
 		}
 	});
 }());
